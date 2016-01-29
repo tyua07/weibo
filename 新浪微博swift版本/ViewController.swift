c8ff3dfd62fedd22f63e93131d1ff53b744bd94f
杨一繁 <547484196@qq.com>
2016-01-29 19:20:20 +0800

    Initial Commit

---
+++ 新浪微博swift版本/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  新浪微博swift版本
+//
+//  Created by yangyifan on 16/1/29.
+//  Copyright © 2016年 weibo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
